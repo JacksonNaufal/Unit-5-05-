37e73769210b6660f56a0ef4dd79c0273fff2fc3
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-18 13:50:49 +0000

    removed line 4

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let number = 0
 const value = 0
 let length = 0
-let area = length * length
 
 document.getElementById('button').addEventListener('click', addition)
 
 function addition () {
   length = document.getElementById('addition').value
-  number= parseInt(area)
+  number = parseInt(value)
   number = length * length
   alert(number)
 }
